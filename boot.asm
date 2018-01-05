5f91652cf950ecb2071f82c6c4dbfe505a76d6e7
scooter <jaskooner.singh@gmail.com>
2018-01-05 15:40:39 +0000

    set up paging in protected mode. We are in compatibility mode

--- boot.asm
+++ boot.asm
@@ -15,6 +15,37 @@ start:
     mov dword [p3_table + 0], eax
     ; point each page table level two entry to a page
     mov ecx, 0         ; counter variable
+.map_p2_table:
+    mov eax, 0x200000  ; 2MiB
+    mul ecx
+    or eax, 0b10000011
+    mov [p2_table + ecx * 8], eax
+
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2_table
+
+    ; move page table address to cr3
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+     ; set the long mode bit
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
+    
     mov word [0xb8000], 0x0248 ; H
     mov word [0xb8002], 0x0265 ; e
     mov word [0xb8004], 0x026c ; l
